1b322cd77bf07c59b4c500c8bec7dfe5ed10aa3e
Ayush <ayushayush.sadwal@gmail.com>
2024-01-23 22:08:53 +0530

    added jenkins and deployment files

---
+++ tf-aks/aks.tf
@@ -0,0 +1,52 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.88.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+    
+  }
+}
+
+resource "azurerm_resource_group" "rg1" {
+  name     = "simple_react_app"
+  location = "West Europe"
+}
+
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "library-app"
+  location            = azurerm_resource_group.rg1.location
+  resource_group_name = azurerm_resource_group.rg1.name
+  dns_prefix          = "aks-dns"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Production"
+  }
+}
+
+output "client_certificate" {
+  value     = azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate
+  sensitive = true
+}
+
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.aks.kube_config_raw
+
+  sensitive = true
+}
